abb993b7b4631cdaa07af72c1a2e3f07d48e9147
wangyong315 <291140738@qq.com>
2025-07-20 12:11:03 +0800

    fix: dev分支测试sourcemap

---
+++ src/views/Preview.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="pre-code">
+        <div class="error-detail">
+            <pre class="error-code" v-html=""></pre>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Preview',
+        props: {
+            origin: Object,
+        },
+        methods: {
+            preLine(){
+                const line = this.origin.line
+                const originCodeLine = this.origin.source.split('/n')
+                const len = originCodeLine.length-1
+                const start = len-3>=0 ? line-3 : 0
+                const end = start+5 > len ? len : start+5
+                let newLines = []
+                for(let i=start;i<=end;i++){
+                    const content = i+1 + '.  ' + this.encodeHTML(originCodeLine[i])
+                    newLines.push(
+                        `<div class='code-line ${i+1 == line ? 'heightlight' : ''}'${content}></div>`
+                    )
+
+                }
+                return newLines?.join('')
+            },
+            encodeHTML(str){
+                if(!str || str.length === 0) return ''
+                return str
+                        .replace(/&/g, '&#38;') 
+                        .replace(/</g, '&lt;') 
+                        .replace(/>/g, '&gt;') 
+                        .replace(/'/g, '&#39;') 
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+.pre-code {
+    .err-detail {
+        .error-code {
+            padding: 10px;
+            overflow: hidden;
+            word-wrap: normal;
+        }
+    }
+    .code-line {
+        padding: 4px;
+    }
+    .heightlight {
+        color: #fff;
+        background-color: red;
+    }
+}
+
+
+</style>
+
